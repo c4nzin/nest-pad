d4504fbebff3b866c698f0fc32c159e322192a06
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-15 01:00:47 +0300

    create nest project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  await app.listen(3000);
+}
+bootstrap();
