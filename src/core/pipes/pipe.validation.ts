a332037eb12b3511a034ecf7246a591b69a150cb
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-15 17:23:39 +0300

    feat: add pipe validation

---
+++ src/core/pipes/pipe.validation.ts
@@ -0,0 +1,40 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { ValidationError, validate } from 'class-validator';
+
+@Injectable()
+export class PipeValidation<T> implements PipeTransform {
+  public async transform(value: T, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+
+    const object = plainToInstance(metatype, value);
+    const errors = await validate(object, { whitelist: true });
+
+    if (errors.length === 0) {
+      return value;
+    }
+
+    const errMessages: string[] = [];
+
+    errors.forEach((error: ValidationError) => {
+      Object.values(error.constraints).forEach((message) => {
+        errMessages.push(message);
+      });
+    });
+
+    throw new BadRequestException(errMessages);
+  }
+
+  public async toValidate(metatype: Function) {
+    const types: Function[] = [Array, Object, String, Boolean, Number];
+
+    return !types.includes(metatype);
+  }
+}
