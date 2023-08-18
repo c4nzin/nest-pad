5fe670b8314510dd7b4b0a6ae97abdd8b023bf64
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-18 00:30:32 -0700

    feat: add user module

---
+++ src/features/user/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './user.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+})
+export class UserModule {}
