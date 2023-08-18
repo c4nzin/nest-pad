8496c5faa3dfe77edde3760ad1110cd2a09d556f
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-18 00:29:59 -0700

    feat: add access token strategy

---
+++ src/features/auth/strategies/accessToken.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Config, ENV } from 'src/config';
+import { JwtPayload } from 'src/core/interfaces/jwt-payload.interface';
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(@Inject(ENV) private readonly configService: Config) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.JWT_ACCESS_SECRET,
+    });
+  }
+
+  public validate(payload: JwtPayload): JwtPayload {
+    return payload;
+  }
+}
+export { JwtPayload };
