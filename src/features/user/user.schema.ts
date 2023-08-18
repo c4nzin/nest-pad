3c1a956837c50471c200ee2f5886a996c61e1cf9
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-18 00:23:53 -0700

    remove: bcrypt middleware

--- src/features/user/user.schema.ts
+++ src/features/user/user.schema.ts
@@ -1,12 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Types, Document } from 'mongoose';
-import bcrypt from 'bcrypt';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User extends Document {
-  @Prop({ unique: true, type: String, trim: true, maxlength: 15, minlength: 3 })
+  @Prop({ unique: true, type: String, trim: true })
   public username: string;
 
   @Prop({
@@ -21,20 +20,14 @@ export class User extends Document {
   @Prop({
     type: String,
     trim: true,
-    maxlength: 15,
-    minlength: 3,
-    select: false,
   })
   public password: string;
 
   @Prop({ type: Types.ObjectId, default: [], ref: 'User' })
   public notes: Types.ObjectId[];
+
+  @Prop()
+  public refreshToken: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
-
-UserSchema.pre('save', function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = bcrypt.hashSync(this.password, 10);
-  return next();
-});
