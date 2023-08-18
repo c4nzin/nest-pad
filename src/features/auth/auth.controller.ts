11a0ceba601bc96641771cc54366ba34f274ab5f
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-18 00:33:35 -0700

    feat: add register action

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
 import { RegisterDto } from './dto';
 import { AuthService } from './auth.service';
-import { UserDocument } from '../user/user.schema';
+import { LoginDto } from './dto';
+import { Message } from 'src/core/decorators/';
+
+import { AccessTokenGuard, RefreshTokenGuard } from 'src/core/guards/';
+import { ApiTags } from '@nestjs/swagger';
+import { TokenResponse } from 'src/core/interfaces';
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
+  @Message('Sucessfully registered')
   @Post('register')
-  public async register(
-    @Body() registerDto: RegisterDto,
-  ): Promise<UserDocument> {
-    return this.authService.register(registerDto);
+  public async register(@Body() createUserDto: RegisterDto): TokenResponse {
+    return await this.authService.register(createUserDto);
   }
 }
