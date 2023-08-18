950c20f06807ab1395734497a64b70ee542afa19
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-18 00:40:02 -0700

    refactor: add new fields in env

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,10 @@ const config = {
   PORT: port({ default: 5000 }),
   DB_URI: str(),
   GLOBAL_PREFIX: str({ default: 'api' }),
+  JWT_ACCESS_SECRET: str(),
+  JWT_REFRESH_SECRET: str(),
+  EXPIRES_IN: str({ default: '15m' }),
+  REFRESH_TIME: str({ default: '1d' }),
 };
 
 export const validators = makeValidators(config);
