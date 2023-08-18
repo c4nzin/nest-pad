0cf2dac5136f8c1c19b5afe31530212adaa6fb12
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-19 02:55:07 +0300

    feat: add note module

--- src/features/note/note.module.ts
+++ src/features/note/note.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { NoteController } from './note.controller';
 import { NoteService } from './note.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Note, NoteSchema } from './note.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: Note.name, schema: NoteSchema }]),
+  ],
   controllers: [NoteController],
   providers: [NoteService],
-  exports: [],
+  exports: [NoteService],
 })
 export class NoteModule {}
