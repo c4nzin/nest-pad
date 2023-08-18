1851e99ed06b8f45dda33971ea3d8a13e5136cd1
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-18 00:35:49 -0700

    feat: add register service

--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -1,15 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from '../user/user.schema';
-import { Model } from 'mongoose';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { RegisterDto } from './dto';
+import { UserService } from '../user/user.service';
+import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+
+import { LoginDto } from './dto';
+import { Config, ENV } from 'src/config';
+import { UserDocument } from '../user/user.schema';
+import { hashData, verifyData } from '../../utils/data-modify'; //will be used in the future
+import { TokenResponse } from 'src/core/interfaces';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userRepository: Model<User>) {}
+  constructor(
+    private readonly usersService: UserService,
+    private readonly jwtService: JwtService,
+    @Inject(ENV) private readonly config: Config,
+  ) {}
 
-  public async register(registerDto: RegisterDto): Promise<UserDocument> {
-    const user = await this.userRepository.create(registerDto);
-    return user;
+  public async register(createUserDto: RegisterDto): Promise<TokenResponse> {
+    const user = await this.usersService.findByUsername(createUserDto.username);
+
+    if (user) throw new BadRequestException('User already exists');
+
+    const hashedPassword = await this.hashData(createUserDto.password);
+    const newUser = await this.usersService.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    const tokens = await this.getTokens(newUser._id, newUser.username);
+    await this.updateRefreshToken(newUser._id, tokens.refreshToken);
+
+    return tokens;
   }
 }
