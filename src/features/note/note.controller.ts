b0297d332c831bb85184cea2b5679cad348dab55
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-19 04:38:51 +0300

    feat: add create new notepad action

--- src/features/note/note.controller.ts
+++ src/features/note/note.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body, Req } from '@nestjs/common';
+import { Message } from 'src/core/decorators';
+import { NoteService } from './note.service';
+import { AccessTokenGuard } from 'src/core/guards';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateNotepadDto } from './dto/create-notepad.dto';
+import { NoteDocument } from './note.schema';
+import { Request } from 'express';
 
-@Controller('note')
-export class NoteController {}
+@Controller('notes')
+@ApiTags('Notes')
+export class NoteController {
+  constructor(private readonly noteService: NoteService) {}
+
+  @Post('create')
+  @Message('Sucessfully created note')
+  @UseGuards(AccessTokenGuard)
+  public async createNote(
+    @Body() createNotepadDto: CreateNotepadDto,
+    @Req() req: Request,
+  ): Promise<NoteDocument> {
+    const currentUsername = req.user['username'];
+    return this.noteService.create(createNotepadDto, currentUsername);
+  }
+}
--- src/features/note/note.service.ts
+++ src/features/note/note.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Note, NoteDocument } from './note.schema';
+import { Model } from 'mongoose';
+import { CreateNotepadDto } from './dto/create-notepad.dto';
+import { User, UserDocument } from '../user/user.schema';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class NoteService {}
+export class NoteService {
+  constructor(
+    @InjectModel(Note.name) private noteModel: Model<Note>,
+    private readonly usersService: UserService,
+  ) {}
+
+  public async create(
+    createNotepadDto: CreateNotepadDto,
+    username: string,
+  ): Promise<any> {
+    const user = await this.usersService.findByUsername(username);
+    const notepad = await this.noteModel.create({
+      ...createNotepadDto,
+      author: user._id,
+    });
+    await this.usersService.updateArray(user._id, notepad._id);
+    return notepad.save();
+  }
+}
