bc3e8becc776dd4b22f7e4d1f065ffcc4b45c96f
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-21 18:28:36 +0300

    feat: add throttler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
 import { EnvalidModule } from 'nestjs-envalid';
-import { validators } from './config';
+import { Config, ENV, validators } from './config';
 import { DatabaseModule } from './modules/database/database.module';
 import { AuthModule } from './features/auth/auth.module';
 import { LoggerModule } from './modules/logger/logger.module';
 import { UserModule } from './features/user/user.module';
 import { NoteModule } from './features/note/note.module';
 import { RouterModule } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRootAsync({
+      inject: [ENV],
+      useFactory: (configService: Config) => ({
+        ttl: configService.THROTTLE_TTL,
+        limit: configService.THROTTLE_LIMIT,
+      }),
+    }),
+
     EnvalidModule.forRoot({
       isGlobal: true,
       validators,
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import { str, port } from 'envalid';
+import { str, port, num } from 'envalid';
 import { makeValidators, Static } from 'nestjs-envalid';
 
 const config = {
@@ -9,6 +9,8 @@ const config = {
   JWT_REFRESH_SECRET: str(),
   EXPIRES_IN: str({ default: '15m' }),
   REFRESH_TIME: str({ default: '1d' }),
+  THROTTLE_TTL: num({ default: 60 }),
+  THROTTLE_LIMIT: num({ default: 10 }),
 };
 
 export const validators = makeValidators(config);
