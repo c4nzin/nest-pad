ace260ada80e2d43570ef6f73936693bb6ca4cb6
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-22 15:07:48 +0300

    refactor: add singular naming convention instead of plural naming

--- src/features/note/note.service.ts
+++ src/features/note/note.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Note, NoteDocument } from './note.schema';
 import { Model } from 'mongoose';
-import { CreateNotepadDto } from './dto/create-notepad.dto';
+import { CreateNotepadDto } from './dto';
 import { UserService } from '../user/user.service';
 import { UserDocument } from '../user/user.schema';
 
@@ -10,40 +10,42 @@ import { UserDocument } from '../user/user.schema';
 export class NoteService {
   constructor(
     @InjectModel(Note.name) private noteModel: Model<NoteDocument>,
-    private readonly usersService: UserService,
+    private readonly userService: UserService,
   ) {}
 
   public async create(
     createNotepadDto: CreateNotepadDto,
     username: string,
   ): Promise<NoteDocument> {
-    const user = await this.usersService.findByUsername(username);
+    const user = await this.userService.findByUsername(username);
+
     const notepad = await this.noteModel.create({
       ...createNotepadDto,
       author: user._id,
     });
-    await this.usersService.updateArray(user._id, notepad._id);
+
+    await this.userService.updateArray(user._id, notepad._id);
     return notepad.save();
   }
 
   public async delete(
-    toDeleteNotepadId: string,
-    requestId: string,
+    notepadId: string,
+    activeUserId: string,
   ): Promise<UserDocument> {
-    const note = await this.noteModel.findById({ _id: toDeleteNotepadId });
-    const user = await this.usersService.findById(requestId);
+    const note = await this.noteModel.findById({ _id: notepadId });
+    const user = await this.userService.findById(activeUserId);
 
     if (!note) {
       throw new BadRequestException('Notepad is not found');
     }
 
-    if (!note.author.equals(requestId)) {
+    if (!note.author.equals(activeUserId)) {
       throw new BadRequestException(
         'You are not authorized to delete this note',
       );
     }
 
-    await this.noteModel.findByIdAndDelete(toDeleteNotepadId);
+    await this.noteModel.findByIdAndDelete(notepadId);
 
     // Somehow this code ain't running. Will be fixed soon
     // NEED TO BE FIXED: await this.usersService.deleteItemFromArray(requestId, toDeleteNotepadId);
