7dc96b17effac453a3e2dab729ad5d5472e9330e
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-22 04:50:30 +0300

    refactor: auth service

--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -1,47 +1,53 @@
-import {
-  BadRequestException,
-  ForbiddenException,
-  Inject,
-  Injectable,
-} from '@nestjs/common';
-import { RegisterDto } from './dto';
-import { UserService } from '../user/user.service';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import * as argon2 from 'argon2';
-import { JwtService } from '@nestjs/jwt';
 
-import { LoginDto } from './dto';
-import { Config, ENV } from 'src/config';
 import { UserDocument } from '../user/user.schema';
-import { hashData, verifyData } from '../../utils/data-modify'; //will be used in the future
-import { TokenResponse } from 'src/core/interfaces';
+import { UserService } from '../user/user.service';
+import { LoginDto, RegisterDto } from './dto';
+import { TokenService } from 'src/helpers/tokens/token.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UserService,
-    private readonly jwtService: JwtService,
-    @Inject(ENV) private readonly config: Config,
+    private readonly tokenService: TokenService,
   ) {}
 
-  public async register(createUserDto: RegisterDto): Promise<TokenResponse> {
-    const user = await this.usersService.findByUsername(createUserDto.username);
+  public async register(registerDto: RegisterDto): Promise<string> {
+    const isUserAlreadyExists = await this.usersService.findByUsername(
+      registerDto.username,
+    );
 
-    if (user) throw new BadRequestException('User already exists');
+    if (isUserAlreadyExists)
+      throw new BadRequestException('User already exists');
 
-    const hashedPassword = await this.hashData(createUserDto.password);
-    const newUser = await this.usersService.create({
-      ...createUserDto,
+    const existingUserEmail = await this.usersService.findByEmail(
+      registerDto.email,
+    );
+
+    if (existingUserEmail)
+      throw new BadRequestException('Email is already registered');
+
+    const hashedPassword = await this.tokenService.hashData(
+      registerDto.password,
+    );
+
+    const createdUser = await this.usersService.create({
+      ...registerDto,
       password: hashedPassword,
     });
 
-    const tokens = await this.getTokens(newUser._id, newUser.username);
-    await this.updateRefreshToken(newUser._id, tokens.refreshToken);
+    const generateTokens = await this.tokenService.generateAndSaveTokens(
+      createdUser._id,
+      createdUser.username,
+    );
 
-    return tokens;
+    return generateTokens.accessToken;
   }
 
-  public async login(loginDto: LoginDto): TokenResponse {
+  public async login(loginDto: LoginDto): Promise<string> {
     const user = await this.usersService.findByUsername(loginDto.username);
+
     if (!user) {
       throw new BadRequestException('User does not exist');
     }
@@ -55,88 +61,15 @@ export class AuthService {
       throw new BadRequestException('Password is not correct');
     }
 
-    const tokens = await this.getTokens(user._id, user.username);
+    const generateTokens = await this.tokenService.generateAndSaveTokens(
+      user._id,
+      user.username,
+    );
 
-    await this.updateRefreshToken(user._id, tokens.refreshToken);
-
-    return tokens;
+    return generateTokens.accessToken;
   }
 
   public async logout(userId: string): Promise<UserDocument> {
     return this.usersService.update(userId, { refreshToken: null });
-  }
-
-  //This field will be removed
-  public async hashData(data: string): Promise<string> {
-    return argon2.hash(data);
-  }
-
-  public async updateRefreshToken(
-    userId: string,
-    refreshToken: string,
-  ): Promise<void> {
-    const hashedRefreshToken = await this.hashData(refreshToken);
-    await this.usersService.update(userId, {
-      refreshToken: hashedRefreshToken,
-    });
-  }
-
-  public async getTokens(userId: string, username: string): TokenResponse {
-    const [accessToken, refreshToken] = await Promise.all([
-      //Generating access token
-      this.jwtService.signAsync(
-        {
-          sub: userId,
-          username,
-        },
-        {
-          secret: this.config.JWT_ACCESS_SECRET,
-          expiresIn: this.config.EXPIRES_IN,
-        },
-      ),
-
-      //Generating refresh jwt
-      this.jwtService.signAsync(
-        {
-          sub: userId,
-          username,
-        },
-        {
-          secret: this.config.JWT_REFRESH_SECRET,
-          expiresIn: this.config.REFRESH_TIME,
-        },
-      ),
-    ]);
-
-    return {
-      accessToken,
-      refreshToken,
-    };
-  }
-
-  public async refreshToken(
-    userId: string,
-    refreshToken: string,
-  ): TokenResponse {
-    const user = await this.usersService.findById(userId);
-
-    if (!user) throw new BadRequestException('User not found');
-
-    if (!user.refreshToken) {
-      throw new ForbiddenException('Refresh token is not found');
-    }
-
-    const isRefreshTokenMatches = await argon2.verify(
-      user.refreshToken,
-      refreshToken,
-    );
-
-    if (!isRefreshTokenMatches) throw new ForbiddenException('Access Denied');
-
-    const tokens = await this.getTokens(user.id, user.username);
-
-    await this.updateRefreshToken(user.id, tokens.refreshToken);
-
-    return tokens;
   }
 }
