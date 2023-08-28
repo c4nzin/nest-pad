c06d3a3d730777493e920f709c511226f28e30b1
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-08-28 17:20:02 +0300

    fix: feature module providers

--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { NoteModule } from './note/note.module';
+import { JwtService } from '@nestjs/jwt';
+import { AccessTokenStrategy, RefreshTokenStrategy } from './auth/strategies';
 
 @Module({
   imports: [AuthModule, UserModule, NoteModule],
   controllers: [],
-  providers: [],
+  providers: [JwtService, RefreshTokenStrategy, AccessTokenStrategy],
   exports: [],
 })
 export class FeatureModule {}
