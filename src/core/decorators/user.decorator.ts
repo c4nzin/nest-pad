25361a931cf0a6a1de1f7890ec5f3fcfc7e7eb24
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-09-01 16:31:15 +0300

    refactor: remove unnecessary variable

--- src/core/decorators/user.decorator.ts
+++ src/core/decorators/user.decorator.ts
@@ -1,13 +1,9 @@
-//Deprecated, will be removed.
-
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
 export const User = createParamDecorator(
   (data: string, context: ExecutionContext): Express.User => {
     const request = context.switchToHttp().getRequest<Request>();
-    const user = request.user;
-
-    return data ? user?.[data] : user;
+    return data ? request.user?.[data] : request.user;
   },
 );
