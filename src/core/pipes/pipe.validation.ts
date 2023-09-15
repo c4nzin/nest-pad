36dbde61af3afb5c269a3f9f15af5e3f5775df60
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-09-15 08:49:59 +0300

    refactor: remove unnecessary loops and replace with more readable code

--- src/core/pipes/pipe.validation.ts
+++ src/core/pipes/pipe.validation.ts
@@ -10,24 +10,16 @@ import { ValidationError, validate } from 'class-validator';
 @Injectable()
 export class PipeValidation<T> implements PipeTransform {
   public async transform(value: T, { metatype }: ArgumentMetadata): Promise<T> {
-    if (!metatype || !this.toValidate(metatype)) {
-      return value;
-    }
+    if (!metatype || !this.toValidate(metatype)) return value;
 
     const object = plainToInstance(metatype, value);
     const errors = await validate(object, { whitelist: true });
 
-    if (errors.length === 0) {
-      return value;
-    }
+    if (errors.length === 0) return value;
 
-    const errMessages: string[] = [];
-
-    errors.forEach((error: ValidationError) => {
-      Object.values(error.constraints).forEach((message) => {
-        errMessages.push(message);
-      });
-    });
+    const errMessages = errors.flatMap((error: ValidationError) =>
+      Object.values(error.constraints),
+    );
 
     throw new BadRequestException(errMessages);
   }
