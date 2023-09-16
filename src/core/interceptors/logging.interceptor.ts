d478aaa5bb5d110406f25532ed6a1ddde843fbc4
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-09-16 19:49:11 +0300

    refactor: improve readablity on logging interceptor

--- src/core/interceptors/logging.interceptor.ts
+++ src/core/interceptors/logging.interceptor.ts
@@ -1,63 +1,52 @@
-import { Logger } from 'nestjs-pino';
 import {
   Injectable,
-  CallHandler,
-  ExecutionContext,
-  HttpException,
-  HttpStatus,
   NestInterceptor,
+  ExecutionContext,
+  CallHandler,
 } from '@nestjs/common';
-import { Response, Request } from 'express';
-import { Observable, tap } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Logger } from 'nestjs-pino';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   constructor(private readonly logger: Logger) {}
 
-  public async intercept(
+  public intercept(
     context: ExecutionContext,
-    next: CallHandler<unknown>,
-  ): Promise<Observable<unknown>> {
-    const ctx = context.switchToHttp();
-    const method = ctx.getRequest<Request>().method;
-    const url = ctx.getRequest<Request>().url;
+    next: CallHandler,
+  ): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const method = request.method;
+    const url = request.url;
 
-    this.logger.log(`Incoming request: ${method} , ${url}`);
+    this.logger.log(`Incoming request: ${method} ${url}`);
 
     return next.handle().pipe(
-      tap({
-        next: () => this.logSuccess(context),
-        error: (error) => this.logFailure(error, context),
+      tap(() => {
+        const response = context.switchToHttp().getResponse();
+        const statusCode = response.statusCode;
+        this.logResponse(method, url, statusCode);
+      }),
+      catchError((error) => {
+        const response = context.switchToHttp().getResponse();
+        const statusCode = response.statusCode;
+        this.logResponse(method, url, statusCode, true);
+        return throwError(error);
       }),
     );
   }
 
-  private logSuccess(context: ExecutionContext): void {
-    const ctx = context.switchToHttp();
-    const { method, url } = ctx.getRequest<Request>();
-    const { statusCode } = ctx.getResponse<Response>();
-
-    const message = `Outgoing Response : ${method} - ${url} - ${statusCode}`;
-
-    this.logger.log(message);
-  }
-
-  private logFailure(error: Error, context: ExecutionContext): void {
-    const ctx = context.switchToHttp().getRequest<Request>();
-    const { method, url } = ctx;
-
-    if (!(error instanceof HttpException)) {
-      const message = `Outgoing Response - ${method} - ${url}`;
-      return this.logger.error(message, error.stack);
-    }
-
-    const statusCode = error.getStatus();
-    const message = `Outgoing response - ${statusCode} - ${method} - ${url}`;
-
-    if (statusCode >= HttpStatus.INTERNAL_SERVER_ERROR) {
-      this.logger.error(message, error.stack);
-    }
-
-    this.logger.warn(message, error.message);
+  private logResponse(
+    method: string,
+    url: string,
+    statusCode: number,
+    isError = false,
+  ): void {
+    const logFn = isError
+      ? this.logger.error.bind(this.logger)
+      : this.logger.log.bind(this.logger);
+    const message = `Outgoing Response: ${method} ${url} Status: ${statusCode}`;
+    logFn(message);
   }
 }
