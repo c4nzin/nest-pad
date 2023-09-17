a6288aa3530a8258e2143e1f929cd8522e88bc94
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-09-17 17:39:48 +0300

    chore: add global app settings

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,16 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { Config, ENV } from './config';
-import { HttpExceptionFilter } from './core/filters';
-import { TransformInterceptor, LoggingInterceptor } from './core/interceptors';
+
+import { setupApp } from './setup-app';
 import { setupSwagger } from './setup-swagger';
-import expressMongoSanitize from 'express-mongo-sanitize';
-import helmet from 'helmet';
-import compression from 'compression';
-import { Logger } from 'nestjs-pino';
-import { PipeValidation } from './core/pipes';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
 
-  const config = app.get<Config>(ENV);
-  const logger = app.get<Logger>(Logger);
-
-  app.use(
-    expressMongoSanitize({
-      allowDots: true,
-      replaceWith: '_',
-      dryRun: true,
-    }),
-  );
-  app.use(helmet());
-  app.use(compression());
-
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalInterceptors(new TransformInterceptor(new Reflector()));
-  app.useGlobalPipes(new PipeValidation());
-  app.setGlobalPrefix(config.GLOBAL_PREFIX);
-
-  app.useGlobalInterceptors(new LoggingInterceptor(logger));
+  setupApp(app);
   setupSwagger(app);
-
-  await app.listen(config.PORT);
 }
 bootstrap();
---
+++ src/setup-app.ts
@@ -0,0 +1,32 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { Config, ENV } from './config';
+import { Logger } from 'nestjs-pino';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
+import compression from 'compression';
+import { HttpExceptionFilter } from './core/filters';
+import { LoggingInterceptor, TransformInterceptor } from './core/interceptors';
+import { PipeValidation } from './core/pipes';
+
+export async function setupApp(app: NestExpressApplication): Promise<void> {
+  const config = app.get<Config>(ENV);
+  const logger = app.get<Logger>(Logger);
+
+  app.use(
+    ExpressMongoSanitize({ allowDots: true, replaceWith: '_', dryRun: true }),
+  );
+
+  app.use(helmet());
+  app.use(compression({ threshold: 0 }));
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor(new Reflector()));
+  app.useGlobalPipes(new PipeValidation());
+  app.useGlobalInterceptors(new LoggingInterceptor(logger));
+
+  app.setGlobalPrefix(config.GLOBAL_PREFIX);
+
+  await app.listen(config.PORT);
+}
