45b2d41b966f5bd513d7df52c7864c59ce28ee0e
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-09-18 15:14:34 +0300

    fix: add missing routes typo

--- src/features/routes.ts
+++ src/features/routes.ts
@@ -1,8 +1,9 @@
+import { Routes } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { NoteModule } from './note/note.module';
 import { UserModule } from './user/user.module';
 
-export const routes = [
+export const routes: Routes = [
   { path: '/auth', module: AuthModule },
   { path: '/notes', module: NoteModule },
   { path: '/users', module: UserModule },
