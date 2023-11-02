1ac95e825dd5792f0dfd88b367fe486ec4238c63
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-11-02 21:52:38 +0300

    fix: undefined cloudinary api key

--- src/features/cloudinary/cloudinary.module.ts
+++ src/features/cloudinary/cloudinary.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryModule as CloudinaryClientModule } from 'nestjs-cloudinary';
-import { Config } from 'src/config';
 
 @Module({
   imports: [
     CloudinaryClientModule.forRootAsync({
-      imports: [],
-      useFactory: (configService: Config) => ({
-        cloud_name: configService.CLOUD_NAME,
-        api_key: configService.CLOUDINARY_API_KEY,
-        api_secret: configService.CLOUDINARY_API_SECRET,
+      isGlobal: true,
+      useFactory: () => ({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
         secure: false,
       }),
     }),
