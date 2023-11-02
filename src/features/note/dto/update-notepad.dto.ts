89d49f2e732dcafd1af2f56a56bf6df5c734e8f4
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-11-02 22:13:20 +0300

    refactor: use partial type

--- src/features/note/dto/update-notepad.dto.ts
+++ src/features/note/dto/update-notepad.dto.ts
@@ -1,23 +1,4 @@
-import {
-  IsString,
-  IsOptional,
-  IsNotEmpty,
-  MinLength,
-  MaxLength,
-} from 'class-validator';
-import { IsValidLength } from '../../../core/decorators';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateNotepadDto } from './create-notepad.dto';
 
-export class UpdateNoteDto {
-  @IsOptional()
-  @IsString({ message: 'Title must be a string' })
-  @IsNotEmpty({ message: 'Title cannot be empty' })
-  @MinLength(3, { message: 'Title must be at least 3 characters long' })
-  @MaxLength(100, { message: 'Title cannot be longer than 100 characters' })
-  @IsValidLength('title', 3, 100)
-  public title?: string;
-
-  @IsOptional()
-  @IsString({ message: 'Content must be a string' })
-  @IsNotEmpty({ message: 'Content cannot be empty' })
-  public content?: string;
-}
+export class UpdateNoteDto extends PartialType(CreateNotepadDto) {}
